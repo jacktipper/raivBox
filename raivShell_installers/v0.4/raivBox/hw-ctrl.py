59584b5074cf37a70d534796e5011b616ade992f
jacktipper <74902963+jacktipper@users.noreply.github.com>
2022-02-04 13:30:10 -0500

    update

---
+++ raivShell_installers/v0.4/raivBox/hw-ctrl.py
@@ -0,0 +1,110 @@
+from time import sleep
+import Jetson.GPIO as GPIO
+import signal
+import subprocess
+import os
+os.system('./init-py.sh') # initialize for python
+os.system('./init-pa.sh') # initialize pulseaudio
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+# set the 'ON' indicator LED to led0 and map it to the correct BCM GPIO number
+led0pin = 20
+GPIO.setup(led0pin, GPIO.OUT, initial=GPIO.LOW)
+
+# blink led0 during the initialization sequence to indicate success
+for i in range(3):
+    GPIO.output(led0pin, GPIO.HIGH)
+    sleep(0.08)
+    GPIO.output(led0pin, GPIO.LOW)
+    sleep(0.12)
+for i in range(4):
+    GPIO.output(led0pin, GPIO.HIGH)
+    sleep(0.04)
+    GPIO.output(led0pin, GPIO.LOW)
+    sleep(0.06)
+sleep(0.26)
+
+# leave led0 on
+GPIO.output(led0pin, GPIO.HIGH)
+
+# map the additional LEDs and buttons to the correct GPIO pins
+led1pin = 21
+button1pin = 27
+led2pin = 16
+button2pin = 26
+# initialize the loop values to 'None' or '0'
+prev1value = None
+prev2value = None
+recording = 0
+playing = 0
+rec = None
+synth = None
+play = None
+
+GPIO.setup(led1pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(button1pin, GPIO.IN)
+GPIO.setup(led2pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(button2pin, GPIO.IN)
+
+directory = os.getcwd()
+rec_cmd = str(
+    "exec arecord -d 20 -t wav -c 1 -f FLOAT_LE -r 16000 audio/input.wav")
+synth_cmd = str(
+    "exec ./run-synth.sh")
+play1cmd = str(
+    "exec aplay audio/output.wav")
+play2cmd = str(
+    "exec aplay audio/input.wav")
+
+try:
+    while True:
+        curr1value = GPIO.input(button1pin)
+        curr2value = GPIO.input(button2pin)
+        if curr1value != prev1value:
+            GPIO.output(led1pin, not curr1value)
+            # 'curr1value == 0' is the on-press condition
+            if curr1value == 0:
+                recording = 1
+                print('Recording In Progress')
+                rec = subprocess.Popen(
+                    [rec_cmd], cwd=directory, stdout=subprocess.PIPE, shell=True)
+                sleep(0.15)
+            # synthesize output audio once recording is complete
+            elif recording == 1 and curr1value == 1:
+                rec.send_signal(signal.SIGINT)
+                print('Recording Ended')
+                recording = 0
+                try:
+                    synth = subprocess.Popen(
+                        [synth_cmd], cwd=directory, stdout=subprocess.PIPE, shell=True)
+                except:
+                    pass
+                print('Timbre Transfer Engaged')
+            prev1value = curr1value
+        if curr2value != prev2value:
+            GPIO.output(led2pin, not curr2value)
+            if curr2value == 0:
+                playing = 1
+                print('Playing Recorded Audio')
+                try:
+                    play = subprocess.Popen(
+                        [play1cmd], cwd=directory, stdout=subprocess.PIPE, shell=True)
+                except:
+                    play = subprocess.Popen(
+                        [play2cmd], cwd=directory, stdout=subprocess.PIPE, shell=True)
+            if playing == 1 and curr2value == 1:
+                play.send_signal(signal.SIGINT)
+                print('Audio Stopped')
+                playing = 0
+            prev2value = curr2value
+        # set the status check interval for the while loop (responsivity)
+        sleep(0.02)
+finally:
+    # purge the temporary files from the Audio folder
+    os.system("rm audio/recorded/*.*")
+    os.system("rm audio/rendered/*.*")
+    # if the program is prematurely terminated, turn on just the two top LEDs to indicate
+    GPIO.output(led0pin, GPIO.LOW)
+    GPIO.output(led1pin, GPIO.HIGH)
+    GPIO.output(led2pin, GPIO.HIGH)
---
+++ raivShell_installers/v0.4/raivBox/raivShell.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# RAIVSHELL for NVIDIA Jetson Nano 2GB - Deep Learning for Music - Installation Sequence v0.3
+#     assembled by Jack Tipper - tipper@nyu.edu - (c) 2022 RAIV - MIT License
+
+# First, put the enclosing folder on the Desktop, and then run the following line in terminal (without the #):
+# sudo chmod +x ~/Desktop/raivShell/raivShell.sh ; sudo ~/Desktop/raivShell/raivShell.sh
+
+clear ; echo '--Initialize RAIVSHELL Installation Sequence for NVIDIA Jetson Nano 2GB--' ; echo ; sleep 1 ; echo '*'
+echo ; sleep 1 ; echo '*' ; echo ; sleep 1 ; echo '*' ; echo ; sleep 1 ; echo '*' ; echo ; sleep 1 ; echo '*'
+cd ~ ; clear ; echo '--User Confirmation--' ; echo ; sleep 1 ; echo '    This full installation takes approximately 3.5 hours.' ; echo
+read -p "    Enter username (case-sensitive): " USER ; echo ; echo "    Got it, your username is: $USER." ; echo ; sleep 1
+read -p "    Enter a VNC password: " PASS ; echo ; echo "    Thanks! Use that password to log in to VNC." ; echo ; sleep 1
+echo '    Additional interactive input will be required in about 5 minutes.' ; echo
+sleep 1 ; echo '*' ; echo ; sleep 1 ; echo '*' ; echo ; sleep 1 ; clear
+echo '--Boosting System to FULL THROTTLE--' ; echo ; echo ; sleep 2
+sudo nvpmodel -m 0 ; sudo jetson_clocks ; cd /etc/X11/app-defaults/ ; sudo sed -i 's/0:10:00/6:00:00/' ./XScreenSaver ; cd ~
+git clone https://github.com/JetsonHacksNano/resizeSwapMemory ; cd resizeSwapMemory ; ./setSwapMemorySize.sh -g 16 ; clear
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+echo '--Updating--' ; echo ; echo ; sleep 2 ; sudo apt update ; sudo apt-mark hold systemd ; clear
+
+echo '--Setting Up VNC Software--' ; echo ; echo ; sleep 2
+sudo apt install vino ; mkdir -p ~/.config/autostart ; cp /usr/share/applications/vino-server.desktop ~/.config/autostart
+gsettings set org.gnome.Vino prompt-enabled false ; gsettings set org.gnome.Vino require-encryption false
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino vnc-password $(echo -n "$PASS"|base64) ; clear
+
+echo '--Adjusting VNC Settings--' ; echo ; echo ; sleep 2 ; cd /etc/X11/
+echo | sudo tee -a xorg.conf ; echo 'Section "Monitor"' | sudo tee -a xorg.confecho '    Identifier "DSI-0"' | sudo tee -a xorg.conf
+echo '    Option    "Ignore"' | sudo tee -a xorg.conf ; echo "EndSection" | sudo tee -a xorg.conf ; echo | sudo tee -a xorg.conf
+echo 'Section "Screen"' | sudo tee -a xorg.conf ; echo '    Identifier  "Default Screen"' | sudo tee -a xorg.conf
+echo '    Monitor "Configured Monitor"' | sudo tee -a xorg.conf ; echo '    Device      "Default Device"' | sudo tee -a xorg.conf
+echo '    SubSection "Display"' | sudo tee -a xorg.conf ; echo "        Depth   24" | sudo tee -a xorg.conf
+echo "        Virtual 1280 720" | sudo tee -a xorg.conf ; echo "    EndSubSection" | sudo tee -a xorg.conf
+echo "EndSection" | sudo tee -a xorg.conf ; clear
+
+echo '--Purging Bloatware--' ; echo ; echo ; sleep 2 ; cd ~
+yes | sudo apt-get purge libreoffice* ; sudo apt-get clean ; clear
+
+echo '--Installing Jetson Inference Module from NVIDIA--' ; echo ; sleep 2
+echo '    Be prepared to make menu selections at the end of this installation stage.' ; sleep 2
+echo '    After selecting your models and installations, there will not be any'
+echo '    additional interactive elements in this script until the final reboot sequence.' ; echo ; sleep 1 
+echo '*' ; echo ; sleep 1 ; echo '*' ; sleep 1 ; echo
+git clone https://github.com/dusty-nv/jetson-inference ; cd jetson-inference ; git submodule update --init
+mkdir build ; cd build ; cmake ../ ; yes | sudo dkpg --configure -a ; clear #gets docker set up properly
+
+
+echo '--Installing Python Libraries and Dependencies--' ; echo ; echo '    The following section takes about 3 hours to complete.'
+echo ; sleep 1 ; echo '*' ; echo ; sleep 1 ; echo '*' ; echo ; sleep 1 ; echo '*' ; echo ; sleep 1 ; echo '*' ; sleep 1 ; echo '*'
+yes | sudo apt install python3-pip python3-pil 
+yes | sudo apt install llvm-7 ; cd /usr/bin ; sudo ln llvm-config-7 llvm-config ; cd ~
+yes | sudo apt-get install git cmake puredata libatlas-base-dev gfortran libhdf5-serial-dev hdf5-tools python3-dev 
+yes | sudo apt-get install nano mlocate libpython3-dev python3-numpy libfreetype6-dev python3-setuptools
+yes | sudo apt-get install protobuf-compiler libprotobuf-dev openssl libssl-dev libcurl4-openssl-dev
+yes | sudo apt-get install cython3 libxml2-dev libxslt1-dev curl libssl1.0-dev nodejs-dev node-gyp nodejs npm
+sudo npm install -g n ; sudo n stable ; sudo updatedb
+sudo -H pip3 install threadpoolctl dominate ; sudo -H pip3 install -U numpy setuptools ; yes | sudo apt-get upgrade
+sudo -H pip3 install llvmlite==0.31 numba==0.48
+sudo -H pip3 install matplotlib ipython colorgram.py scikit-build ; sudo -H pip3 install librosa pandas ; clear
+
+echo '--Installing DDSP--' ; echo ; echo ; sleep 2
+sudo apt-get update ; sudo apt-get install libblas-dev liblapack-dev liblapack-doc
+sudo -H pip3 install -U pip testresources setuptools==49.6.0 protobuf
+sudo -H pip3 install -U numpy==1.19.4 mock==3.0.5 h5py==2.10.0 keras_preprocessing==1.1.1 # h5py takes a while
+sudo -H pip3 install -U keras_applications==1.0.8 gast==0.2.2 futures pybind11
+sudo -H pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v46 tensorflow
+sudo -H npm install -g @bazel/bazelisk # needed for `dm-tree` in DDSP
+sudo -H pip3 install -U ddsp colorama ; clear
+
+echo '--Installing ADC Connections for Analog Control--' ; echo ; echo ; sleep 2
+sudo -H pip3 install adafruit-circuitpython-mcp3xxx Adafruit-Blinka
+cd ../ ; cd raivCtrl ; sudo chmod +x init-spi.sh ; sudo chmod +x init-pa.sh ; sudo chmod +x init-py.sh ; clear
+
+echo '--Creating OLED Status Display Service--' ; echo ; echo ; sleep 2
+git clone https://github.com/JetsonHacksNano/installPiOLED
+cd installPiOLED; ./installPiOLED.sh; ./createService.sh
+sudo scp ~/Desktop/raivBox/raivCtrl/stats.py /usr/local/lib/python3.6/dist-packages/pioled
+cd ~ ; sudo rm -rf installPiOLED ; sudo rm ~/Desktop/raivBox/raivCtrl/stats.py
+sudo systemctl enable pioled_stats ; sudo systemctl start pioled_stats ; clear
+
+echo '--Creating Analog Control systemd Modules--' ; echo ; echo ; sleep 2
+sudo touch /etc/systemd/user/boot-leds.service
+sudo cat ~/Desktop/raivBox/raivCtrl/boot-leds_service.txt > /etc/systemd/user/boot-leds.service
+sudo chmod +x /etc/systemd/user/boot-leds.service ; 
+systemctl --user enable boot-leds ; systemctl --user start boot-leds ; clear
+sudo touch /etc/systemd/system/boot-knobs.service
+sudo cat ~/Desktop/raivBox/raivCtrl/boot-knobs_service.txt > /etc/systemd/system/boot-knobs.service
+sudo chmod +x /etc/systemd/system/boot-knobs.service ; 
+sudo systemctl enable boot-knobs ; sudo systemctl start boot-knobs ; clear
+
+echo '--Final Housekeeping--' ; echo ; sleep 2
+sudo apt update ; yes | sudo apt upgrade ; sudo -H pip3 install -U jetson-stats ; yes | sudo apt-get install florence
+cd ~/Desktop ; touch florence.sh ; echo "#!/bin/bash" | sudo tee -a florence.sh
+echo "echo 'Launching Florence Virtual Keyboard'" | sudo tee -a florence.sh
+echo "florence" | sudo tee -a florence.sh; sudo chmod +x florence.sh
+yes | sudo apt autoremove ; sudo apt clean ; clear
+
+echo '--SPI Configuration and Reboot Sequence--' ; echo ; sleep 2
+echo '    First, please resize this terminal window to be much larger on your screen.' ; sleep 1
+echo '    Then, in the next screen, select "Configure 40-pin expansion header"'
+echo '    at the bottom of the menu.' ; sleep 1
+echo '    After that, make sure [*] spi1 (19,21,23,24,26) is selected.' ; sleep 1
+echo '    Then select "Back" at the bottom, save, and reboot.' ; sleep 1
+read -p "    When you are ready to proceed to these steps, please press [ENTER]." x ; sudo /opt/nvidia/jetson-io/jetson-io.py
+
+# End of Shell
